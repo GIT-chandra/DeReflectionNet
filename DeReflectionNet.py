692c50c521c070a7b27e639f571f72fefb537221
Angom Geetchandra <angomgeetchandra@gmal.com>
2017-11-05 15:22:59 +0530

    Data Generator

--- DeReflectionNet.py
+++ DeReflectionNet.py
@@ -2,17 +2,25 @@ from vgg16 import VGG16
 from keras.preprocessing import image
 from imagenet_utils import preprocess_input
 from keras.models import Model
+from Classes_DeReflectionNet import *
 import numpy as np
 
-base_model = VGG16(weights='imagenet')
-vgg_A = Model(input=base_model.input, output=base_model.get_layer('block3_pool').output)
-vgg_S = Model(input=base_model.input, output=base_model.get_layer('block5_pool').output)
+class DeReflectionNet:
+    def __init__(self,dim_x = 224, dim_y = 224, dim_z = 5, batch_size = 16, shuffle = True, auxiliary = False):
+        self.data_params = {'dim_x': dim_x,
+                        'dim_y':dim_y,
+                        'dim_z':dim_z,
+                        'batch_size':batch_size,
+                        'shuffle':shuffle,
+                        'auxiliary':auxiliary}
+        base_model = VGG16(weights='imagenet')
+        self.vgg_A = Model(input=base_model.input, output=base_model.get_layer('block3_pool').output)
+        self.vgg_S = Model(input=base_model.input, output=base_model.get_layer('block5_pool').output)
+        self.partition = {}
+        self.load_data()
+        self.training_generator = DataGenerator(**params).generate(self.partition['train'])
+        self.validation_generator = DataGenerator(**params).generate(self.partition['validation'])
+        self.net = self.get_model()
 
-img_path = 'cat.jpg'
-img = image.load_img(img_path, target_size=(224, 224))
-x = image.img_to_array(img)
-x = np.expand_dims(x, axis=0)
-x = preprocess_input(x)
-
-vggA_features = vgg_A.predict(x)
-vggS_features = vgg_S.predict(x)
+    def load_data(self):
+        return
